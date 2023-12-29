d0c95b81d5ce3a2953e9fde082f4562a4c3c03b9
AliRagab7777 <ali.ragab2520@gmail.com>
2023-12-29 04:25:28 +0200

    Added delete page and some validations.

---
+++ Admin/Delete Car Page/deleteCar.php
@@ -0,0 +1,68 @@
+<?php
+session_start(); // Start the session
+include '../../db_connect.php'; // Include the database connection file
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $platenumber = $_POST['car-plate-number'];
+    $sql = "UPDATE car SET car_status = 'out of service' , date_deleted = CURRENT_TIMESTAMP  where (plate_number = '$platenumber' and car_status != 'out of service');";
+    if($conn->query($sql) === TRUE)
+    {
+        //if the plate number does not exist in the DB.
+        if($conn->affected_rows == 0)
+        {
+            echo '<script type="text/javascript">
+            var userResponse = confirm("This car plate number does not exist in the database, So it is not deleted!");
+                window.location.href = "deleteCar.php";
+            
+          </script>';
+        }
+
+
+        else
+        {
+            echo '<script type="text/javascript">
+            var userResponse = confirm("The car is successfully deleted!");
+                window.location.href = "deleteCar.php";
+            
+          </script>';
+        }
+    }
+     else {
+        echo "The car is not deleted!";
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    
+}
+
+    $conn->close(); // Close the database connection
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Delete Car Page</title>
+    <link rel="stylesheet" href="deleteCar.css">
+</head>
+<body>
+ <main>
+    <header>
+        <h1>Car Rental System</h1>
+    </header>
+
+    <div class="form-container">
+        <form id="deletecar-form" method = "post" action = "deleteCar.php">
+                <label for="car-plate-number">Enter Plate Number:</label>
+                <input type="text" id="car-plate-number" name="car-plate-number" required>
+
+                <button type="submit">Delete Car</button>
+    </form>
+</div>
+
+</main>
+    <footer>
+        <p>&copy; 2023 Car Rental System</p>
+    </footer>
+
+</body>
+</html>
