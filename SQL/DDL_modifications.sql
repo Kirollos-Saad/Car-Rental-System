ce38758577a8c7d7cf08d853fc1586942d905be2
AliRagab7777 <ali.ragab2520@gmail.com>
2024-01-02 02:09:56 +0200

    added pickup.

--- SQL/DDL_modifications.sql
+++ SQL/DDL_modifications.sql
@@ -1,7 +1,7 @@
 /* Allow Date Deleted to be NULL*/
 
 ALTER TABLE Car
-MODIFY COLUMN date_deleted TIMESTAMP NULL;
+MODIFY COLUMN date_deleted DATE NULL;
 
 
 /*Set date deleted for all cars in the system to be NULL
@@ -11,18 +11,21 @@ UPDATE Car
 SET date_deleted = NULL;
 
 /* Add Date Added column and allow it to be NULL */
+/*ALTER TABLE Car
+ADD COLUMN date_added DATE NULL;*/
+
 ALTER TABLE Car
-ADD COLUMN date_added TIMESTAMP NULL;
+MODIFY COLUMN date_added DATE NULL;
 
 /* Set date_added for all cars in the system to be the current timestamp */
 UPDATE Car
-SET date_added = "2023-01-01 23:09:17";
+SET date_added = "2023-01-01";
 
 
 /* Allow Pickup Date to be NULL*/
 
 ALTER TABLE Current_Renting
-MODIFY COLUMN pick_up_date TIMESTAMP NULL;
+MODIFY COLUMN pick_up_date DATE NULL;
 
 
 /*Set pickup date deleted for all cars in the system to be NULL
